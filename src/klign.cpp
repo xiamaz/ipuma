231417e86cd05810f59b65b1391e39e73cd157ac
Steven Hofmeyr <shofmeyr@lbl.gov>
2021-03-15 17:58:38 -0400

    fixed cache resizing issues

--- src/klign.cpp
+++ src/klign.cpp
@@ -484,7 +484,7 @@ class KmerCtgDHT {
 #else
     gpu_mem_avail = 32 * 1024 * 1024;  // cpu needs a block of memory
 #endif
-    ctg_cache.reserve(2 * all_num_ctgs / rank_n());
+    ctg_cache.reserve(all_num_ctgs / rank_n() * 2);
 
 #ifdef USE_KMER_CACHE
     if (!first_ctg_round) kmer_cache.reserve(KLIGN_KMER_CACHE_SIZE);
@@ -506,8 +506,8 @@ class KmerCtgDHT {
   int64_t size() const { return kmer_map->size(); }
 
   intrank_t get_target_rank(const Kmer<MAX_K> &kmer, const Kmer<MAX_K> *kmer_rc = nullptr) const {
-    return kmer.minimizer_hash_fast(15, kmer_rc) % rank_n();
-    // return std::hash<Kmer<MAX_K>>{}(kmer) % rank_n();
+    //return kmer.minimizer_hash_fast(15, kmer_rc) % rank_n();
+    return std::hash<Kmer<MAX_K>>{}(kmer) % rank_n();
   }
 
   int64_t get_num_kmers(bool all = false) {
