81a97e537d99de12716a9822248eeffc0e640552
Steven Hofmeyr <shofmeyr@lbl.gov>
2021-08-26 02:05:00 -0400

    default modules for summit for latest environment

--- contrib/environments/summit/gnu.sh
+++ contrib/environments/summit/gnu.sh
@@ -1,12 +1,13 @@
 module use /gpfs/alpine/world-shared/csc296/summit/modulefiles
 
-module load git/2.20.1
-module load cmake/3.18.2
+module load git
+module load cmake
 
 module rm xl
-module load gcc/7.4.0
-module load cuda/10.1.243
+module load gcc
+module load cuda
 module load upcxx
+module load python
 
 export MHM2_CMAKE_EXTRAS="-DCMAKE_C_COMPILER=$(which mpicc) -DCMAKE_CXX_COMPILER=$(which mpicxx) -DCMAKE_CUDA_COMPILER=$(which nvcc)"
 export MHM2_BUILD_THREADS=8
