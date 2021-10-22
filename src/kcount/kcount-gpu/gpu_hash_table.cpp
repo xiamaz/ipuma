0477165512181b934e878239c048e5fd14aba53c
Steven Hofmeyr <shofmeyr@lbl.gov>
2021-10-22 16:15:05 -0400

    give a bit more space to the ctg kmers ht

--- src/kcount/kcount-gpu/gpu_hash_table.cpp
+++ src/kcount/kcount-gpu/gpu_hash_table.cpp
@@ -564,7 +564,7 @@ void HashTableGPUDriver<MAX_K>::init_ctg_kmers(int max_elems, size_t gpu_avail_m
   dstate->qf = nullptr;
   size_t elem_buff_size = KCOUNT_GPU_HASHTABLE_BLOCK_SIZE * (1 + sizeof(count_t)) * 1.5;
   size_t elem_size = sizeof(KmerArray<MAX_K>) + sizeof(CountsArray);
-  size_t max_slots = 0.9 * (gpu_avail_mem - elem_buff_size) / elem_size;
+  size_t max_slots = (gpu_avail_mem - elem_buff_size) / elem_size;
   primes::Prime prime;
   prime.set(min(max_slots, (size_t)(max_elems * 3)), false);
   auto ht_capacity = prime.get();
