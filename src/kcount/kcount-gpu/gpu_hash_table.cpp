116d16ff6b968aca18aa7ab71d9b9f6778e0c64c
Steven Hofmeyr <shofmeyr@lbl.gov>
2021-12-02 13:52:18 -0800

    use a single thread per block in supermer insertion

--- src/kcount/kcount-gpu/gpu_hash_table.cpp
+++ src/kcount/kcount-gpu/gpu_hash_table.cpp
@@ -507,6 +507,10 @@ void HashTableGPUDriver<MAX_K>::insert_supermer_block() {
   get_kernel_config(buff_len, gpu_unpack_supermer_block, gridsize, threadblocksize);
   gpu_unpack_supermer_block<<<gridsize, threadblocksize>>>(unpacked_elem_buff_dev, packed_elem_buff_dev, buff_len);
   get_kernel_config(buff_len * 2, gpu_insert_supermer_block<MAX_K>, gridsize, threadblocksize);
+  //if (upcxx_rank_me == 0) printf("insert supermer gridsize %d threadblocksize %d\n", gridsize, threadblocksize);
+  gridsize = gridsize * threadblocksize;
+  threadblocksize = 1;
+  //if (upcxx_rank_me == 0) printf("insert supermer gridsize %d threadblocksize %d\n", gridsize, threadblocksize);
   gpu_insert_supermer_block<<<gridsize, threadblocksize>>>(is_ctg_kmers ? ctg_kmers_dev : read_kmers_dev, unpacked_elem_buff_dev,
                                                            buff_len * 2, kmer_len, is_ctg_kmers, gpu_insert_stats);
   // the kernel time is not going to be accurate, because we are not waiting for the kernel to complete
